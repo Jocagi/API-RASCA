7a927afa0b87b2400d93c752d3ddd3defbe3f1a2
Allan Davila <allan.davila@hotmail.com>
2021-09-03 10:30:28 -0600

    Update User.java
    Cambio de atributos reales de la base de datos.

--- rasca-api/src/main/java/com/rasca/rascaapi/domain/User.java
+++ rasca-api/src/main/java/com/rasca/rascaapi/domain/User.java
@@ -1,12 +1,100 @@
 package com.rasca.rascaapi.domain;
 
-public class User {
-    private Integer userId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
+import java.util.Date;
 
+public class User {
+    private Integer IDPersona;
+    private String Correo;
+    private String Contrasena;
+    private String Usuario;
+    private String Nombres;
+    private String Apellidos;
+    private String Carnet;
+    private String FechaNac;
+    private String Telefono;
+    private String Fotografia;
+
+    public void setIDPersona(Integer IDPersona) {
+        this.IDPersona = IDPersona;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public void setContrasena(String contrasena) {
+        Contrasena = contrasena;
+    }
+
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+
+    public void setNombres(String nombres) {
+        Nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public void setCarnet(String carnet) {
+        Carnet = carnet;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        FechaNac = fechaNac;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public void setFotografia(String fotografia) {
+        Fotografia = fotografia;
+    }
+
+    public Integer getIDPersona() {
+        return IDPersona;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public String getContrasena() {
+        return Contrasena;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public String getCarnet() {
+        return Carnet;
+    }
+
+    public String getFechaNac() {
+        return FechaNac;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public String getFotografia() {
+        return Fotografia;
+    }
+
+    /*
     public User(Integer userId, String firstName, String lastName, String email, String password) {
         this.userId = userId;
         this.firstName = firstName;
@@ -53,5 +141,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
+    }*/
 }
