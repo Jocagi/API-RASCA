60604862d77cb9a34613a7cfeb56e80b49ed9793
Allan Davila <allan.davila@hotmail.com>
2021-09-19 22:02:04 -0600

    Update ActivityResource.java
    Implementacion de servicio de creacion de actividades

--- rasca-api/src/main/java/com/rasca/rascaapi/resources/ActivityResource.java
+++ rasca-api/src/main/java/com/rasca/rascaapi/resources/ActivityResource.java
@@ -1,6 +1,9 @@
 package com.rasca.rascaapi.resources;
 
+import com.rasca.rascaapi.domain.Activities;
 import com.rasca.rascaapi.domain.User;
+import com.rasca.rascaapi.services.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +20,23 @@ public class ActivityResource {
         return "Usuario Autenticado! UserId: " + userId;
     }
 
+
+
+    @Autowired
+    ActivityService activityService;
     @PostMapping("/create")
-    public ResponseEntity<Map<String,String>> create(@RequestBody Map<String,Object> userMap) {
-        String Correo = (String) userMap.get("Correo");
-        String Contrasena = (String) userMap.get("Contrasena");
-     //   User user = userService.validateUser(Correo,Contrasena);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Map<String,String>> create(HttpServletRequest request, @RequestBody Map<String,Object> categoryMap) {
+        int IDPersona = (int) request.getAttribute("IDPersona");
+        String ID_Certificador = Integer.toString(IDPersona);
+        String Nombre = (String) categoryMap.get("Nombre");
+        int Cupo= (int) categoryMap.get("Cupo");
+        String Fecha_Inicio = (String) categoryMap.get("Fecha_Inicio");
+        String Descripcion = (String) categoryMap.get("Descripcion");
+        int Horas_Otorgadas= (int) categoryMap.get("Horas_Otorgadas");
+        String R_Facultad = (String) categoryMap.get("R_Facultad");
+        String R_Year = (String) categoryMap.get("R_Year");
+        String R_Beca = (String) categoryMap.get("R_Beca");
+        Activities activity = activityService.agregaActividad(Nombre,Cupo,Fecha_Inicio,Descripcion,Horas_Otorgadas,R_Facultad,R_Year,R_Beca,ID_Certificador, 1L);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
