8fdaab10de0bb2c60c9dfc50be5584db5d672f03
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-10-31 21:19:18 -0600

    Devolver rol de usuario en login

--- src/main/java/com/rasca/rascaapi/resources/UserResource.java
+++ src/main/java/com/rasca/rascaapi/resources/UserResource.java
@@ -1,7 +1,11 @@
 package com.rasca.rascaapi.resources;
 
 import com.rasca.rascaapi.Constants;
+import com.rasca.rascaapi.domain.Administrator;
+import com.rasca.rascaapi.domain.Approver;
+import com.rasca.rascaapi.domain.Student;
 import com.rasca.rascaapi.domain.User;
+import com.rasca.rascaapi.services.ActivityService;
 import com.rasca.rascaapi.services.UserService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -22,6 +26,8 @@ import java.util.Map;
 public class UserResource {
     @Autowired
     UserService userService;
+    @Autowired
+    ActivityService activityService;
 
     @PostMapping("/login")
     public ResponseEntity<Map<String,String>> login(@RequestBody Map<String,Object> userMap) {
@@ -55,15 +61,46 @@ public class UserResource {
 
     private Map<String,String> generateJWTToken (User user){
         long timestamp = System.currentTimeMillis();
+        String rol = getUserRole(user);
         String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY())
                 .setIssuedAt(new Date(timestamp))
                 .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
                 .claim("IDPersona", user.getIDPersona())
                 .claim("Correo", user.getCorreo())
                 .claim("Carnet", user.getCarnet())
+                .claim("Rol", rol)
                 .compact();
         Map<String, String> map = new HashMap<>();
         map.put("token",token);
+        map.put("Rol", rol);
         return map;
+    }
+
+    private String getUserRole(User user) {
+        long id = user.getIDPersona();
+        String rol = "Undefined";
+        Approver certificador = null;
+        Student estudiante = null;
+        Administrator admin = null;
+        try{
+            certificador = activityService.obtenerCertificador(id);
+        }catch (Exception e){}
+        try{
+            estudiante = activityService.obtenerEstudiante(id);
+        }catch (Exception e){}
+        try{
+            admin = activityService.obtenerAdministrador(id);
+        }catch (Exception e){}
+
+        if (admin != null) {
+            rol = "Administrador";
+        }
+        else if (estudiante != null) {
+            rol = "Estudiante";
+        }
+        else if (certificador != null) {
+            rol = "Certificador";
+        }
+        return rol;
     }
 }
