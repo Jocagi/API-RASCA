107b73ca2d25a5b4e2e85d8632144c51cb1830cc
Allan Davila <allan.davila@hotmail.com>
2021-11-11 21:34:19 -0600

    Update TestUsers.java
    Implementado testing en registro de usuarios

--- src/main/java/com/rasca/rascaapi/resources/TestUsers.java
+++ src/main/java/com/rasca/rascaapi/resources/TestUsers.java
@@ -34,6 +34,31 @@ public class TestUsers{
         assertTrue(response.getStatusCode().is2xxSuccessful());
         assertTrue(response.getBody().contains("token"));
     }
+    @Test
+    public void testRegister() throws Exception{
+        Map<String,Object> map = Map.ofEntries(
+                Map.entry("Correo","try2@rasca.com"),
+                Map.entry("Contrasena", "password"),
+                Map.entry("Usuario", "tray2"),
+                Map.entry("Nombres", "Raymond"),
+                Map.entry("Apellidos", "Holt"),
+                Map.entry("Carnet", "1160118"),
+                Map.entry("FechaNac", "23/08/1999"),
+                Map.entry("Telefono", "55361555"),
+                Map.entry("Fotografia", "-"),
+                Map.entry("Rol","Certificador"),
+                Map.entry("IDCarrera","1"),
+                Map.entry("IDBeca","2"),
+                Map.entry("IDCargo","1")
+        );
+        HttpEntity<Map<String,Object>> entity = new HttpEntity<Map<String,Object>>(map, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/api/users/register"),
+                HttpMethod.POST, entity, String.class);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertTrue(response.getBody().contains("Registrado exitosamente"));
+    }
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
     }
